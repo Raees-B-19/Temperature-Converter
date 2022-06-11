fc67b5f1042ad6e1e8add4d12ae43a28d69be1ff
GarlicBuns19 <planterx26@gmail.com>
2022-06-11 14:59:54 +0100

    help

--- code.js
+++ code.js
@@ -3,10 +3,7 @@ let convert = document.getElementById('convert');
 
 convert.addEventListener('click',() => {
     let fahrenheit = document.querySelector('#fahrenheit');
-    let celsius = document.querySelector('celsius');
     let f = fahrenheit;
-    
-    
 
     let converter = eval((f.value - 32) / 1.8);
     // .value to target input value
@@ -15,5 +12,5 @@ convert.addEventListener('click',() => {
     let answer = converter.toFixed(4);
     // console.log(answer)
 
-    answer.value = c.value
+    
 })
