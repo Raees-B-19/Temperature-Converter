cbfc9783d9b625868cbed3020cd1d7dd59afa7ee
GarlicBuns19 <planterx26@gmail.com>
2022-06-11 15:16:34 +0100

    Done

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-
+// Fahrenheit to Celsius 
 let convert = document.getElementById('convert');
 
 convert.addEventListener('click',() => {
@@ -13,4 +13,21 @@ convert.addEventListener('click',() => {
     // console.log(answer)
 
     document.getElementById('celsius').innerHTML = answer
+})
+
+// Celsius to Fahrenheit
+let convert1 = document.getElementById('convert1');
+
+convert1.addEventListener('click',() => {
+    let celsius  = document.querySelector('#celsius1');
+    let c = celsius ;
+
+    let converter1 = eval((c.value * 1.8) + 32);
+    // .value to target input value
+
+    // toFixed(x) is to round off decimals
+    let answer1 = converter1.toFixed(4);
+    // console.log(answer)
+
+    document.getElementById('fahrenheit1').innerHTML = answer1
 })
