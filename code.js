022cd8b71ff82fc26a9f08bb23288398f8444fb8
GarlicBuns19 <planterx26@gmail.com>
2022-06-11 14:17:55 +0100

    plz hep

--- code.js
+++ code.js
@@ -1,2 +1,11 @@
-let fahrenheit = document.querySelector('#fahrenheit');
-let celsius = document.querySelector('celsius');
+let convert = document.getElementById('convert');
+
+convert.addEventListener('click' , () => {
+    let fahrenheit = document.querySelector('#fahrenheit');
+    let celsius = document.querySelector('celsius');
+
+    switch(convert) {
+        case (fahrenheit): 
+    }
+})
+
