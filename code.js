e60ac06bc00cd2eaeba92582241a3f27aa729b43
GarlicBuns19 <planterx26@gmail.com>
2022-06-11 14:31:11 +0100

    Almost done

--- code.js
+++ code.js
@@ -1,11 +1,14 @@
+let fahrenheit = document.querySelector('#fahrenheit');
+let celsius = document.querySelector('celsius');
+
+let f = fahrenheit;
+let c = celsius;
+
 let convert = document.getElementById('convert');
 
-convert.addEventListener('click' , () => {
-    let fahrenheit = document.querySelector('#fahrenheit');
-    let celsius = document.querySelector('celsius');
-
-    switch(convert) {
-        case (fahrenheit): 
-    }
+convert.addEventListener('click',() => {
+    let converter = eval((f.value - 32) / 1.8);
+    // toFixed(x) is to round off decimals
+    let answer = converter.toFixed(4)
+    // console.log(answer)
 })
-
